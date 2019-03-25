a5a8d38f19dad2ab53ba19a23ea2a6b01b8dc875
tjf4412 <tjf4412@g.rit.edu>
2019-03-24 22:07:41 -0400

    Undoing PHP

--- assets/inc/footer.php
+++
@@ -1,9 +0,0 @@
-        <div class="box footer">
-            <p>
-                Website Created by Trevor Froggatt, GFA
-            </p>
-        </div>
-    </div>
-<script> </script>
-</body>
-</html>
